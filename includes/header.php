3735f161f058a01603e29f8bbda38be0f4c504f7
Amit Kushwaha <akkushwaha018@gmail.com>
2024-11-24 20:35:44 +0530

    first commit

---
+++ includes/header.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Indore Call Girl</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+  <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-0 header" id="header">
+        <div class="container">
+            <a class="navbar-brand py-0" href="./">
+                <span class="h2 fw-semibold">
+                    <img src="assets/images/isha.png" class="img-fluid" width="40%">
+                </span>
+            </a>
+            <button class="navbar-toggler border-0" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end bgc-primary" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <div class="offcanvas-header">
+                    <a href="#">
+                        <span class="h2 fw-semibold">LOGO</span>
+                    </a>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <div class="navbar-collapse">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-lg-3">
+                            <li class="nav-item">
+                                <a class="nav-link" href="./">
+                                    Home
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="about.php">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="profile.php">Profile</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="rate.php">Rate</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="service.php">Services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact-us.php">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,138 @@
+<?php include_once('./includes/header.php') ?>
+<section class="escorts-list">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h3 class="text-center" style="color: #d3af56;">
+					Gallery of Our Finest Call Girls in Haryana
+				</h3>
+				<p class="text-white text-center gallery-para">
+					Welcome to the secret and glittery world of Delhi storing sheer gladness and satisfaction for you. In the dark stressed and strained life, we are the only nucleus centring peace of mind and happiness for males. 
+				</p>
+			</div>
+		</div>
+		<div class="row mt-3 gy-4 text-center">
+			<div class="col-md-3">
+				<a data-target="#myModal1" data-toggle="modal" type="button" class="text-decoration-none text-white">
+					<div class="escorts-lising text-center">
+						<img src="assets/images/booknow.webp" class="img-fluid" width="80%">
+						<div class="images-box">
+							<img src="assets/images/heena.webp" class="img-fluid"  width="100%">
+							<div class="image-box-with-text">
+			             		<h4 class="text-center mb-0">
+			               	 		Muskan
+			              		</h4>
+			            	</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-3">
+				<a data-target="#myModal2" data-toggle="modal" type="button" class="text-decoration-none text-white">
+					<div class="escorts-lising text-center">
+						<img src="assets/images/booknow.webp" class="img-fluid" width="80%">
+						<div class="images-box">
+							<img src="assets/images/ishika.webp" class="img-fluid"  width="100%">
+							<div class="image-box-with-text">
+			             		<h4 class="text-center mb-0">
+			               	 		Payal Pathak
+			              		</h4>
+			            	</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-3">
+				<a data-target="#myModal3" data-toggle="modal" type="button" class="text-decoration-none text-white">
+					<div class="escorts-lising text-center">
+						<img src="assets/images/booknow.webp" class="img-fluid" width="80%">
+						<div class="images-box">
+							<img src="assets/images/kiara.webp" class="img-fluid"  width="100%">
+							<div class="image-box-with-text">
+			             		<h4 class="text-center mb-0">
+			               	 		Sikha
+			              		</h4>
+			            	</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-3">
+				<a data-target="#myModal4" data-toggle="modal" type="button" class="text-decoration-none text-white">
+					<div class="escorts-lising text-center">
+						<img src="assets/images/booknow.webp" class="img-fluid" width="80%">
+						<div class="images-box">
+							<img src="assets/images/lipika.webp" class="img-fluid"  width="100%">
+							<div class="image-box-with-text">
+			             		<h4 class="text-center mb-0">
+			               	 		Sonika Joshi
+			              		</h4>
+			            	</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-3">
+				<a data-target="#myModal5" data-toggle="modal" type="button" class="text-decoration-none text-white">
+					<div class="escorts-lising text-center">
+						<img src="assets/images/booknow.webp" class="img-fluid" width="80%">
+						<div class="images-box">
+							<img src="assets/images/mayuri.webp" class="img-fluid"  width="100%">
+							<div class="image-box-with-text">
+			             		<h4 class="text-center mb-0">
+			               	 		 Anjali Gupta
+			              		</h4>
+			            	</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-3">
+				<a data-target="#myModal6" data-toggle="modal" type="button" class="text-decoration-none text-white">
+					<div class="escorts-lising text-center">
+						<img src="assets/images/booknow.webp" class="img-fluid" width="80%">
+						<div class="images-box">
+							<img src="assets/images/mia.webp" class="img-fluid"  width="100%">
+							<div class="image-box-with-text">
+			             		<h4 class="text-center mb-0">
+			               	 		Diya Sharma
+			              		</h4>
+			            	</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-3">
+				<a data-target="#myModal7" data-toggle="modal" type="button" class="text-decoration-none text-white">
+					<div class="escorts-lising text-center">
+						<img src="assets/images/booknow.webp" class="img-fluid" width="80%">
+						<div class="images-box">
+							<img src="assets/images/muskan.webp" class="img-fluid"  width="100%">
+							<div class="image-box-with-text">
+			             		<h4 class="text-center mb-0">
+			               	 		Simran Singh
+			              		</h4>
+			            	</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-3">
+				<a data-target="#myModal8" data-toggle="modal" type="button" class="text-decoration-none text-white">
+					<div class="escorts-lising text-center">
+						<img src="assets/images/booknow.webp" class="img-fluid" width="80%">
+						<div class="images-box">
+							<img src="assets/images/priya.webp" class="img-fluid"  width="100%">
+							<div class="image-box-with-text">
+			             		<h4 class="text-center mb-0">
+			               	 		Kavita Singh
+			              		</h4>
+			            	</div>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include_once('./includes/footer.php') ?>
